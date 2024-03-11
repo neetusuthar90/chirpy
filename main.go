9a81732331ca7660a684b47ed6da5b8109552561
Neetu Kumari <neetusuthar90@gmail.com>
2024-03-11 14:56:12 +0530

    Hands on JSON object

--- main.go
+++ main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type apiConfig struct {
 	fileserverHits int
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
 }
 
 func main() {
@@ -28,15 +20,30 @@ func main() {
 	apiCfg := apiConfig{
 		fileserverHits: 0,
 	}
-	mux := http.NewServeMux()
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	//mux.Handle("/app/", http.FileServer(http.Dir(".")))
 
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handleMetrics)
-	mux.HandleFunc("/reset", apiCfg.handleReset)
+	r := chi.NewRouter()
+	r.Use(corsMiddleWare)
 
-	corsMux := corsMiddleWare(mux)
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	r.Handle("/app/*", fsHandler)
+	r.Handle("/app", fsHandler)
+
+	r.Get("/api/healthz", handlerReadiness)
+	r.Get("/api/reset", apiCfg.handlerReset)
+
+	r.Get("/admin/metrics", apiCfg.handlerMetrics)
+
+	r.Post("/api/validate_chirp", handleChirpyValidation)
+
+	// mux := http.NewServeMux()
+	// mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	// //mux.Handle("/app/", http.FileServer(http.Dir(".")))
+
+	// mux.HandleFunc("/healthz", handlerReadiness)
+	// mux.HandleFunc("/metrics", apiCfg.handleMetrics)
+	// mux.HandleFunc("/reset", apiCfg.handleReset)
+
+	corsMux := corsMiddleWare(r)
 
 	server := &http.Server{
 		Addr:    ":" + port,
@@ -47,4 +54,74 @@ func main() {
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		fmt.Printf("Error starting server: %s\n", err)
 	}
+}
+
+func handleChirpyValidation(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+	const maxChirpyLength = 140
+	if len(params.Body) > maxChirpyLength {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	badWords := map[string]struct{}{
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	cleanedText := getCleanedText(params.Body, badWords)
+
+	respondWithJSON(w, http.StatusOK, returnVals{
+		CleanedBody: cleanedText,
+	})
+}
+
+func getCleanedText(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+
+	for i, word := range words {
+		lowerWord := strings.ToLower(word)
+		if _, ok := badWords[lowerWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %s", msg)
+	}
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorResponse{
+		Error: msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(dat)
 }
